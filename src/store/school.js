82a08067842eb658d402148ffce66a117c7398e6
CraftyDuck100 <61465936+CraftyDuck100@users.noreply.github.com>
2024-10-05 14:33:11 -0500

    Update school.js

--- src/store/school.js
+++ src/store/school.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import store from "../store"
 import { SCHOOL_BOOK_BASE_GAIN, SCHOOL_EXAM_DUST_MIN, SCHOOL_EXAM_PASS_PRICE } from "../js/constants";
 
 
@@ -107,9 +108,9 @@ export default {
             commit('updateKey', {key: 'bonusDust', value: 0});
             commit('updateKey', {key: 'multipass', value: 1});
         },
-        convertPass({ getters, rootGetters, dispatch, rootState }) {
+        convertPass({ getters, rootGetters, dispatch }) {
             if (rootGetters['currency/value']('school_examPass') >= 1) {
-                dispatch('currency/gain', {feature: 'school', name: 'goldenDust', amount: Math.min(SCHOOL_EXAM_DUST_MIN, rootState.stat.school_highestGrade.total) * getters.dustMult}, {root: true});
+                dispatch('currency/gain', {feature: 'school', name: 'goldenDust', amount: Math.min(SCHOOL_EXAM_DUST_MIN, store.stat.school_highestGrade.total) * getters.dustMult}, {root: true});
                 dispatch('currency/spend', {feature: 'school', name: 'examPass', amount: 1}, {root: true});
             }
         },
