398e08af6f9bc73053f9d423134beacf2c83c25e
CraftyDuck100 <61465936+CraftyDuck100@users.noreply.github.com>
2024-10-05 14:38:08 -0500

    Update school.js

--- src/store/school.js
+++ src/store/school.js
@@ -110,7 +110,7 @@ export default {
         },
         convertPass({ getters, rootGetters, dispatch }) {
             if (rootGetters['currency/value']('school_examPass') >= 1) {
-                dispatch('currency/gain', {feature: 'school', name: 'goldenDust', amount: Math.min(SCHOOL_EXAM_DUST_MIN, store.stat.school_highestGrade.total) * getters.dustMult}, {root: true});
+                dispatch('currency/gain', {feature: 'school', name: 'goldenDust', amount: Math.min(SCHOOL_EXAM_DUST_MIN, store.state.stat.school_highestGrade.total) * getters.dustMult}, {root: true});
                 dispatch('currency/spend', {feature: 'school', name: 'examPass', amount: 1}, {root: true});
             }
         },
