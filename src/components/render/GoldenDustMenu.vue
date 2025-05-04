01e2603a821b08d129678101e9b32bc879e7c5a7
CraftyDuck100 <61465936+CraftyDuck100@users.noreply.github.com>
2025-05-04 10:33:53 -0500

    Update GoldenDustMenu.vue

--- src/components/render/GoldenDustMenu.vue
+++ src/components/render/GoldenDustMenu.vue
@@ -113,7 +113,7 @@ export default {
       if (this.canAfford) {
         const module = {mining, village, horde, farm, gallery}[this.$store.state.system.screen];
         module.tick(Math.round(this.minutes * 60 / module.tickspeed));
-        gem.tick(Math.round(this.minutes * 60 / module.tickspeed));
+        gem.tick(Math.round(this.minutes * 60 / gem.tickspeed));
         this.$store.dispatch('currency/spend', {feature: 'school', name: 'goldenDust', amount: 0});
       }
     },
